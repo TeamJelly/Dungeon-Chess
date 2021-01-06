63299ea5f9faf406be03d840af49a5fd51d2ed74
NaTure77 <xnaudrornfl@gmail.com>
2021-01-06 16:18:52 +0900

    UIManager 생성

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class UIManager: MonoBehaviour
+{
+    public static UIManager instance;
+    public TMP_FontAsset TMP_FontAsset;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
