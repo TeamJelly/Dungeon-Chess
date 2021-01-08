dfe2fc6b04cb11d941b1fc5887f7efda0f9ae9b7
Unagi11 <unagi11@naver.com>
2021-01-09 05:36:39 +0900

    stage생성기 작업중

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -11,6 +11,12 @@ public class StageManager : MonoBehaviour
     public Room[,] AllRooms; // 10층, 각층에는 5개
     public Room finalRoom;
 
+    public int numberOfShop;
+    public int numberOfTavern;
+    public int numberOfEvent;
+    public int numberOfElite;
+    public int numberOfMonster;
+
     private void Awake()
     {
         instance = this;
@@ -44,30 +50,30 @@ public class StageManager : MonoBehaviour
             int rand;
             Room currentRoom = AllRooms[0, i];
 
-            while (currentRoom.position.x != AllRooms.GetLength(0)) // 층수가 13층이 될때까지 반복
+            while (currentRoom.position.x != AllRooms.GetLength(0) - 1) // 층수가 13층이 될때까지 반복
             {
-                Debug.LogError(currentRoom.position);
+//                Debug.LogError(currentRoom.position);
 
                 if (currentRoom.position.y == 0)
-                    rand = Random.Range(0, 1);
+                    rand = Random.Range(0, 2);
                 else if (currentRoom.position.y == AllRooms.GetLength(1) - 1) // 맨 오른쪽 방이라면
-                    rand = Random.Range(-1, 0);
-                else // 그 사이라면
                     rand = Random.Range(-1, 1);
+                else // 그 사이라면
+                    rand = Random.Range(-1, 2);
 
-                if (rand == 0 && currentRoom.center != null)
+                if (rand == 0 && currentRoom.center == null)
                 {
                     currentRoom.center = AllRooms[currentRoom.position.x + 1, currentRoom.position.y];
                     currentRoom = currentRoom.center;
                     currentRoom.isActivate = true;
                 }
-                else if (rand == 1 && currentRoom.right != null)
+                else if (rand == 1 && currentRoom.right == null)
                 {
                     currentRoom.right = AllRooms[currentRoom.position.x + 1, currentRoom.position.y + 1];
                     currentRoom = currentRoom.right;
                     currentRoom.isActivate = true;
                 }
-                else if (rand == -1 && currentRoom.left != null)
+                else if (rand == -1 && currentRoom.left == null)
                 {
                     currentRoom.left = AllRooms[currentRoom.position.x + 1, currentRoom.position.y - 1];
                     currentRoom = currentRoom.left;
@@ -93,20 +99,40 @@ public class StageManager : MonoBehaviour
                 if (AllRooms[i, j].isActivate)
                     numberOfRooms++;
 
-        for (int i = 0; i < AllRooms.GetLength(0); i++)
+        for (int i = 0; i < AllRooms.GetLength(1); i++)
         {
             if (AllRooms[0, i].isActivate) // 1층 방은 전투방이다.
+            {
                 AllRooms[0, i].SetRoomCategory(Room.Category.Monster);
-            if (AllRooms[(AllRooms.GetLength(0) + 1) / 2, i].isActivate) // 중간층은 보물방이다.
+                numberOfRooms--;
+            }
+            else if (AllRooms[(AllRooms.GetLength(0) + 1) / 2, i].isActivate) // 중간층은 보물방이다.
+            {
                 AllRooms[(AllRooms.GetLength(0) + 1) / 2, i].SetRoomCategory(Room.Category.Treasure);
-            if (AllRooms[AllRooms.GetLength(0) - 1, i].isActivate) // 보스 전층은 휴식방이다.
+                numberOfRooms--;
+            }
+            else if (AllRooms[AllRooms.GetLength(0) - 1, i].isActivate) // 보스 전층은 휴식방이다.
+            {
                 AllRooms[AllRooms.GetLength(0) - 1, i].SetRoomCategory(Room.Category.Tavern);
+                numberOfRooms--;
+            }
         }
 
         finalRoom.SetRoomCategory(Room.Category.Boss);
 
-        Debug.LogError(numberOfRooms);
+        numberOfShop = (numberOfRooms * 5 + 99) / 100; // 올림 계산
+        numberOfTavern = (numberOfRooms * 10 + 99) / 100;
+        numberOfEvent = (numberOfRooms * 20 + 99) / 100;
+        numberOfElite = (numberOfRooms * 10 + 99) / 100;
+        numberOfMonster = numberOfRooms - numberOfShop - numberOfTavern - numberOfEvent - numberOfElite;
 
+        Debug.LogError("numberOfShop : " + numberOfShop);
+        Debug.LogError("numberOfTavern : " + numberOfTavern);
+        Debug.LogError("numberOfEvent : " + numberOfEvent);
+        Debug.LogError("numberOfElite : " + numberOfElite);
+        Debug.LogError("numberOfMonster : " + numberOfMonster);
+
+        StageUI.instance.InitStage();
     }
 
 
--- Assets/Scripts/UI/StageUI.cs
+++ Assets/Scripts/UI/StageUI.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StageUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static StageUI instance;
+
+    public GameObject contentPanel;
+    public GameObject linePrefab;
+    public GameObject RoomPrefab;
+    public GameObject[] startPosition;
+    public List<GameObject> floors;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitStage()
     {
-        
+        for (int i = 0; i < StageManager.instance.AllRooms.GetLength(0); i++)
+        {
+            GameObject floor = Instantiate(new GameObject(), contentPanel.transform);
+
+            floor.name = "floor " + i;
+            VerticalLayoutGroup verticalLayoutGroup =  floor.AddComponent<VerticalLayoutGroup>();
+
+            verticalLayoutGroup.childAlignment = TextAnchor.MiddleCenter;
+            verticalLayoutGroup.childControlHeight = false;
+            verticalLayoutGroup.childControlWidth = false;
+            verticalLayoutGroup.childForceExpandHeight = true;
+            verticalLayoutGroup.childForceExpandWidth = true;
+
+            floors.Add(floor);
+
+            for (int j = 0; j < StageManager.instance.AllRooms.GetLength(1); j++)
+                if (StageManager.instance.AllRooms[i, j].isActivate)
+                    Instantiate(RoomPrefab, floor.transform);
+        }
     }
+
+    public void UpdateStage()
+    {
+        StageManager.instance.AllRooms.GetLength(0);
+
+    }
+
+    private void DrawLine()
+    {
+        LineRenderer lineRenderer = Instantiate(linePrefab).GetComponent<LineRenderer>();
+
+        int numberOfFloor = StageManager.instance.AllRooms.GetLength(0);
+
+
+
+    }
+
 }
