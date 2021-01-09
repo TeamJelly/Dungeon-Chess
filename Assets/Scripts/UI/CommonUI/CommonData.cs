1978d3b9044ccd510ce6d00f04bd98dc8870e4a2
JeHwanYoo <yoojehwan1875@gmail.com>
2021-01-10 02:36:16 +0900

    #22 Common UI 구축
    프리펩 완성

---
+++ Assets/Scripts/UI/CommonUI/CommonData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CommonData : MonoBehaviour
+{
+    [Serializable]
+    public enum Size
+    {
+        XS, S, M, L, XL
+    }
+
+    [Serializable]
+    public struct SizeField
+    {
+        public float width;
+        public float height;
+    }
+}
