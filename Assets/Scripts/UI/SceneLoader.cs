35761e4b3ad001a52dac42fb07e8fa1a2885baa2
NaTure77 <xnaudrornfl@gmail.com>
2021-01-10 22:32:45 +0900

    MenuManager, SceneLoader 스크립트 생성

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//추후 수정 예정
+public class SceneLoader : MonoBehaviour
+{
+    public static void MoveScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
