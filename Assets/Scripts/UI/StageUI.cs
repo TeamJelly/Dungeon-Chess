c88309e6bb045aef17813e686257a465198828f3
NaTure77 <xnaudrornfl@gmail.com>
2021-01-11 01:09:09 +0900

    스테이지UI 작업
    1. 현재 파티가 위치한 방 표시
    2. 파티가 지나온 방 표시
    3. 다음으로 갈수 있는 방과 못가는방 구분

--- Assets/Scripts/UI/StageUI.cs
+++ Assets/Scripts/UI/StageUI.cs
@@ -11,8 +11,11 @@ public class StageUI : MonoBehaviour
     public GameObject floorPrefab;
     public GameObject roomPrefab;
     public GameObject linePrefab;
+    public GameObject clearSignPrefab;
 
-    public GameObject[,] AllRoomButtons;
+    public Button[,] AllRoomButtons;
+
+    public Transform selector;
 
     public List<GameObject> floors;
     public List<GameObject> lines;
@@ -24,9 +27,54 @@ public class StageUI : MonoBehaviour
         instance = this;
     }
 
+    public void SelectRoom(int x, int y)
+    {
+        //이전에 방문했던 방이 있다면 방문했음을 표시.
+        if (StageManager.instance.currentRoom != null)
+        {
+            Vector2Int pos = StageManager.instance.currentRoom.position;
+            Image image = AllRoomButtons[pos.x, pos.y].GetComponent<Image>();
+            Color color = image.color / 4; color.a = 1;
+            image.color = color;
+
+            Transform t = Instantiate(clearSignPrefab, AllRoomButtons[pos.x, pos.y].transform).transform;
+            t.position = AllRoomButtons[pos.x, pos.y].transform.position;
+        }
+
+        DisableRoomButtons();//일단 모든 방 비활성화
+
+        //현재 방 갱신 및 기록 추가
+        StageManager.instance.currentRoom = StageManager.instance.AllRooms[x, y];
+        StageManager.instance.roomHistory.Add(new Vector2Int(x,y));
+       
+        //현재 방 위치 표시기 갱신
+        selector.SetParent(AllRoomButtons[x, y].transform);
+        selector.position = AllRoomButtons[x, y].transform.position;
+
+        AllRoomButtons[x, y].enabled = false; // 현재 방 버튼 비활성화
+
+        //이동 가능한 방 활성화
+        Room roomChecker = StageManager.instance.AllRooms[x, y].left;
+        if (roomChecker != null) AllRoomButtons[roomChecker.position.x, roomChecker.position.y].interactable = true;
+        roomChecker = StageManager.instance.AllRooms[x, y].center;
+        if (roomChecker != null) AllRoomButtons[roomChecker.position.x, roomChecker.position.y].interactable = true;
+        roomChecker = StageManager.instance.AllRooms[x, y].right;
+        if (roomChecker != null) AllRoomButtons[roomChecker.position.x, roomChecker.position.y].interactable = true;
+    }
+
+    void DisableRoomButtons()
+    {
+        
+    for(int i = 0; i < AllRoomButtons.GetLength(0); i++)
+        for(int j = 0; j < AllRoomButtons.GetLength(1); j++)
+            if(AllRoomButtons[i,j] != null)
+            {
+                AllRoomButtons[i, j].interactable = false;
+            }
+    }
     public void InitStageUI()
     {
-        AllRoomButtons = new GameObject[StageManager.instance.AllRooms.GetLength(0), StageManager.instance.AllRooms.GetLength(1)];
+        AllRoomButtons = new Button[StageManager.instance.AllRooms.GetLength(0), StageManager.instance.AllRooms.GetLength(1)];
 
         for (int i = 0; i < StageManager.instance.AllRooms.GetLength(0); i++)
         {
@@ -39,9 +87,14 @@ public class StageUI : MonoBehaviour
             {
                 if (StageManager.instance.AllRooms[i, j].isActivate)
                 {
-                    GameObject roomButton = Instantiate(roomPrefab, floor.transform);
-                    roomButton.GetComponent<Image>().sprite = roomImages[StageManager.instance.AllRooms[i,j].category.GetHashCode()];
+                    Button roomButton = Instantiate(roomPrefab, floor.transform).GetComponent<Button>();
+                    roomButton.GetComponent<Image>().sprite = roomImages[StageManager.instance.AllRooms[i, j].category.GetHashCode()];
+
+                    //각 방의 버튼마다 이벤트 부여
+                    int x = i, y = j;
+                    roomButton.GetComponent<Button>().onClick.AddListener(() => SelectRoom(x, y));
                     AllRoomButtons[i, j] = roomButton;
+                    if (i != 0) AllRoomButtons[i, j].interactable = false;
                 }
             }
         }
@@ -50,7 +103,6 @@ public class StageUI : MonoBehaviour
         {
             lines.Add(Instantiate(linePrefab));
         }
-
     }
 
     private void Update()
@@ -61,7 +113,6 @@ public class StageUI : MonoBehaviour
     public void UpdateStage()
     {
         StageManager.instance.AllRooms.GetLength(0);
-
     }
 
     public void UpdateLines()
