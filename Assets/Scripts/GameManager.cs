72a0ad4743d3f8c79d0a9c74ae011e0474858560
NaTure77 <xnaudrornfl@gmail.com>
2021-01-14 23:37:05 +0900

    GameManager 생성

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+
+    static GameManager instance;
+
+    public List<Vector2Int> roomHistory = new List<Vector2Int>();
+    public Room currentRoom = null;
+    public Room[,] AllRooms = null;
+    public List<Vector2Int>[] pathList = null;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new GameManager();
+            return instance;
+        }
+    }
+}
