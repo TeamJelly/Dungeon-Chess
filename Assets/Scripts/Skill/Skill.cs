f8b1f1a9e25df84d49b704d916c9e8bc28919b12
NaTure77 <xnaudrornfl@gmail.com>
2021-01-19 20:46:48 +0900

    #16 스킬 타일 제외범위 등호 추가

--- Assets/Scripts/Skill/Skill.cs
+++ Assets/Scripts/Skill/Skill.cs
@@ -143,7 +143,7 @@ public class Skill : MonoBehaviour
             {
                 if (position.x < 0 || position.y < 0) // 범위 제한
                     continue;
-                if (position.x > BattleManager.instance.AllTiles.GetLength(1) || position.y > BattleManager.instance.AllTiles.GetLength(0)) // 범위 제한
+                if (position.x >= BattleManager.instance.AllTiles.GetLength(1) || position.y >= BattleManager.instance.AllTiles.GetLength(0)) // 범위 제한
                     continue;
                 if (target == Target.NoUnitTile && !BattleManager.instance.AllTiles[position.x, position.y].IsUsable())
                     continue;
