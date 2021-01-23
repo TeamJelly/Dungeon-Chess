03a3e3b309a0f0552964634a5d603db5dfeff02b
JeHwanYoo <yoojehwan1875@gmail.com>
2021-01-23 16:13:47 +0900

    JSON.cs 추가

---
+++ Assets/Scripts/Common/JSON.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Common
+{
+    /// <summary>
+    /// JSON 관련 유틸리티 클래스 입니다.
+    /// </summary>
+    public class JSON
+    {
+        public static T ParseFile<T>(string path)
+        {
+            var jsonData = Resources.Load(path) as TextAsset;
+            return JsonUtility.FromJson<T>(jsonData.ToString());
+        }
+        public static T ParseAsset<T>(TextAsset jsonAsset)
+        {
+            return JsonUtility.FromJson<T>(jsonAsset.ToString());
+        }
+        private JSON() { } // Script에 의해 생성될 수 없다.
+    }
+}
