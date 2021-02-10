978adfc7803fc1b0b62291c8b024da754d4fe15b
Unagi11 <unagi11@naver.com>
2021-02-10 17:53:07 +0900

    ui에 유닛정보 조금 추가

--- Assets/Scripts/UI/Battle/UnitInfoUI.cs
+++ Assets/Scripts/UI/Battle/UnitInfoUI.cs
@@ -18,10 +18,22 @@ namespace UI.Battle
         public void Set(Unit unit)
         {
             allocatedUnit = unit;
+
+            string effectNames = "";
+            foreach (var effect in unit.StateEffects)
+            {
+                effectNames += $"({effect.name}) ";
+            }
+
             SetText(
-                $"{unit.Name} \n" +
-                $"HP : {unit.CurrentHP} / {unit.MaximumHP}" +
-                $"Agility : {unit.Agility}"
+                $"{unit.Name}\n" +
+                $"{unit.Level}\n" +
+                $"{unit.CurrentHP}/{unit.MaximumHP}\n" +
+                $"{unit.CurrentEXP}/{unit.NextEXP}\n" +
+                $"{unit.Strength}\n" +
+                $"{unit.Agility}\n" +
+                $"{unit.Move}\n" +
+                $"{effectNames}"
                 );
             unitImage.Sprite = unit.Sprite;
         }
