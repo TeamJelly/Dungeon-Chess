ad86665d5713413257b635547632b36da8b46e79
JeHwanYoo <yoojehwan1875@gmail.com>
2021-02-20 16:36:03 +0900

    DB 수정, Unit Storage 생성

---
+++ Assets/Scripts/Model/Units/UnitStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using Common.DB;
+
+namespace Model.Units
+{
+    public class UnitStorage : Storage<int, Unit>
+    {
+        private static UnitStorage instance = new UnitStorage();
+        public static UnitStorage Instance => instance;
+        private UnitStorage() { }
+    }
+}
