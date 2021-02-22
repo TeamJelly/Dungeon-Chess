e8833716c6b82ace85f8b74db528e377af0a2e56
NaTure77 <xnaudrornfl@gmail.com>
2021-02-22 19:56:22 +0900

    HUD 텍스트 수정 기능 추가

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -13,9 +13,16 @@ namespace UI
 
         // Start is called before the first frame update
 
+        public static HUD instance;
         private void Awake()
         {
+            instance = this;
             text.text = $"골드: {GameManager.Instance.Gold}                  스테이지: {GameManager.Instance.stage}";
+        }
+
+        public static void SetText(string text)
+        {
+            instance.text.text = text;
         }
 
         public void EnableMenu()
