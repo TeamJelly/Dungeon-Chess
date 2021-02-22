53ded2faa8c138437e9ee17d10a6ba7b6d68f5ec
NaTure77 <xnaudrornfl@gmail.com>
2021-02-22 22:34:58 +0900

    이벤트방(야영) 추가

---
+++ Assets/Scripts/Model/Managers/EventRoomManager.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model.Managers
+{
+    public class EventRoomManager : MonoBehaviour
+    {
+        public void HealHP(int value)
+        {
+            foreach (Unit unit in GameManager.PartyUnits)
+            {
+                if (unit.CurrentHP + value > unit.MaximumHP)
+                    value = unit.MaximumHP - unit.CurrentHP;
+
+                unit.CurrentHP += value;
+            }
+
+            MenuManager.instance.GotoStage();
+
+        }
+
+        public void UpgradeSkill()
+        {
+            foreach (Unit unit in GameManager.PartyUnits)
+            {
+                UnitAction.EnhanceSkill(unit, 0);
+            }
+
+            MenuManager.instance.GotoStage();
+        }
+    }
+}
