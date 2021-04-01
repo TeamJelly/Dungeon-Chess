ebd7b7fe0e68e0a0b2d67bf01eea0b80bb328a70
Unagi11 <unagi11@naver.com>
2021-04-02 00:11:13 +0900

    카메라 이동 조금 구현

---
+++ Assets/BattleCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCamera : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseMoveRange = 10f;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Debug.LogError($"{Input.mousePosition}, {Screen.width} {Screen.height}");
+
+        if (Input.mousePosition.x <= mouseMoveRange)
+            horizontal = -1;
+        if (Input.mousePosition.x >= Screen.width - mouseMoveRange)
+            horizontal = 1;
+        if (Input.mousePosition.y <= mouseMoveRange)
+            vertical = -1;
+        if (Input.mousePosition.y >= Screen.height - mouseMoveRange)
+            vertical = 1;
+
+        Vector3 arrow = new Vector2(horizontal, vertical).normalized;
+
+        transform.position += arrow * Time.deltaTime * speed;
+
+    }
+}
