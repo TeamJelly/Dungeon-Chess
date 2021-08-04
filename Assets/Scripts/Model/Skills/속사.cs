ab2440b53d4e855629369a74d5bbdb9b10442255
NaTure77 <xnaudrornfl@gmail.com>
2021-08-04 20:02:21 +0900

    초급 스킬 클래스 생성

---
+++ Assets/Scripts/Model/Skills/속사.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model.Skills
+{
+    public class 속사 : Skill
+    {
+        public 속사()
+        {
+            Name = "화염구";
+            //Number = 1;
+            MaxLevel = 4;
+            ReuseTime = 1;
+            CriticalRate = 10;
+
+            Priority = Common.AI.Priority.NULL;
+            Target = TargetType.Any;
+            Range = RangeType.Fixed;
+
+            Sprite = Common.Data.LoadSprite("HandMade/SkillImage/001_속사");
+            Description =
+                $"지정한 타일 위의 모든 대상에게 데미지를 준다.\n\n\n" +
+                $"‘화염구 관련 대사.’";
+
+            APData = "3;010;101;010";
+            RPData = "1;1";
+
+            species.Add(UnitSpecies.SmallBeast);
+            species.Add(UnitSpecies.MediumBeast);
+            species.Add(UnitSpecies.Human);
+
+            Category = SkillCategory.NormalAttack;
+        }
+
+        private float strToDmg = 1;
+
+        public override void Upgrade()
+        {
+            base.Upgrade();
+
+            switch (Level)
+            {
+                case 1:
+                    strToDmg = 1;
+                    ReuseTime = 1;
+                    break;
+                case 2:
+                    strToDmg = 1.5f;
+                    ReuseTime = 1;
+                    break;
+                case 3:
+                    strToDmg = 1.5f;
+                    ReuseTime = 0;
+                    break;
+                case 4:
+                    strToDmg = 2;
+                    ReuseTime = 0;
+                    break;
+            }
+        }
+        public override IEnumerator Use(Unit user, Vector2Int target)
+        {
+            // 0 단계 : 로그 출력, 스킬 소모 기록, 필요 변수 계산
+            user.SkillCount--;
+            CurReuseTime = ReuseTime;
+
+            int damage = (int)(user.Strength * strToDmg);
+
+            Unit targetUnit = Model.Managers.BattleManager.GetUnit(target);
+            if (targetUnit != null)
+            {
+                Debug.Log($"{user.Name}가 {Name}스킬을 {targetUnit.Name}에 사용!");
+
+                // 1단계 : 스킬 애니메이션 재생 및 화면 갱신.
+                yield return null;
+
+                // 2단계 : 스킬 적용
+                Common.Command.Damage(targetUnit, damage);
+            }
+            else
+            {
+                Debug.Log($"{user.Name}가 {Name}스킬을 {target}에 사용!");
+            }
+        }
+        public override string GetDescription(Unit user, int level)
+        {
+            int damage = (int)(user.Strength * strToDmg);
+            string str = base.GetDescription(user, level).Replace("X", damage.ToString());
+            return str;
+        }
+    }
+}
