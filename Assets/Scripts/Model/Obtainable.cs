6c11d788d026658bf681b49a5914558fd937ba65
Unagi11 <unagi11@naver.com>
2022-06-18 18:41:20 +0900

    remove all

--- Assets/Scripts/Model/Obtainable.cs
+++
@@ -1,8 +0,0 @@
-﻿using Model;
-using UnityEngine;
-
-public interface Obtainable : Infoable, Buyable
-{
-    Vector2Int? Position{get; set;}
-    //void BelongTo(Unit unit);
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-## Dungeon Chess
-
-Dungeon Chess(구 RSRPG)는 로그라이크 방식의 SRPG 게임이다.
